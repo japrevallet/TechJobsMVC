293ee96a24911fd608443ad1d83bf321ca2732f2
Jesse Prevallet <japrevallet@gmail.com>
2018-06-08 20:43:03 -0500

    Its Time

--- src/TechJobs/Controllers/ListController.cs
+++ src/TechJobs/Controllers/ListController.cs
@@ -11,7 +11,8 @@ namespace TechJobs.Controllers
 
         // This is a "static constructor" which can be used
         // to initialize static members of a class
-        static ListController() 
+
+        static ListController()   // this is building the contructor that we call later #01
         {
             
             columnChoices.Add("core competency", "Skill");
@@ -21,24 +22,24 @@ namespace TechJobs.Controllers
             columnChoices.Add("all", "All");
         }
 
-        public IActionResult Index()
+        public IActionResult Index() // this method presents they user with the different column values that the user can select from to make thier List.
         {
             ViewBag.columns = columnChoices;
             return View();
         }
 
-        public IActionResult Values(string column)
+        public IActionResult Values(string column) // takes column value from query parameter
         {
             if (column.Equals("all"))
             {
-                List<Dictionary<string, string>> jobs = JobData.FindAll();
+                List<Dictionary<string, string>> jobs = JobData.FindAll(); // builds a list to hold all of the jobs to be given to the user
                 ViewBag.title =  "All Jobs";
                 ViewBag.jobs = jobs;
-                return View("Jobs");
+                return View("Jobs"); // renders View Jobs template instead of the default view
             }
             else
             {
-                List<string> items = JobData.FindAll(column);
+                List<string> items = JobData.FindAll(column); // builds a list to hold only the values of the column.
                 ViewBag.title =  "All " + columnChoices[column] + " Values";
                 ViewBag.column = column;
                 ViewBag.items = items;
--- src/TechJobs/Controllers/SearchController.cs
+++ src/TechJobs/Controllers/SearchController.cs
@@ -8,13 +8,30 @@ namespace TechJobs.Controllers
     {
         public IActionResult Index()
         {
-            ViewBag.columns = ListController.columnChoices;
+            ViewBag.columns = ListController.columnChoices; // the ListController has a static controllor of default hardcoded columns #01 to iterate through using columnChoices. each value gets rendered to the user through columns.
             ViewBag.title = "Search";
             return View();
         }
 
         // TODO #1 - Create a Results action method to process 
         // search request and display results
+        [HttpPost]
+        public IActionResult Results(string searchType, string searchTerm) //method to display search result 
+        {
+            if (searchType.Equals("all")) //tests to see if keywords should be compared to all Values for all Keys. 
+            {
+                List<Dictionary<string, string>> searchResults = JobData.FindByValue(searchTerm); // builds a list to store the data of the successful results
+                ViewBag.jobs = searchResults; // sends list to Views to be rendered to the user
 
+            }
+            else  // assumes keyword to match columnChoices then compares specific keyword to specific Value from requested Key.
+            {
+                List<Dictionary<string, string>> searchResults = JobData.FindByColumnAndValue(searchType, searchTerm); //builds a list to store the data of the successful results
+                ViewBag.jobs = searchResults; // renders results to user by sending it to Views 
+            }
+            ViewBag.columns = ListController.columnChoices; // the ListController has a static controllor of default hardcoded columns #01 to iterate through using columnChoices. each value gets passed to the View through columns.
+            ViewBag.title = "Search by " + ListController.columnChoices[searchType] + ": " + searchTerm;
+            return View("Index"); // this passes our result to Views/Search/Index.cshtml
+        }
     }
 }
